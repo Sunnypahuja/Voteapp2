dab10732f3c4fcd476c43fa0375a4bba2d0c93c7
Sunny Pahuja <sunny.pahuja89@gmail.com>
2021-04-30 14:08:29 -0700

    first commit

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const UserSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+    email:String,
+    phone: Number,
+    id: Number
+}) ;
+UserSchema.plugin(passportLocalMongoose); //also auto salt and hash password
+module.exports = mongoose.model("User",UserSchema);
